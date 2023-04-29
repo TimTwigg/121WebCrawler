497f09d41e676304bf96e111fb86e59cee2fb4a0
Arthur <83632146+ChairmanArthur@users.noreply.github.com>
2023-04-29 06:39:49 -0700

    Update frontier.py

--- crawler/frontier.py
+++ crawler/frontier.py
@@ -12,6 +12,13 @@ class Frontier(object):
         self.logger = get_logger("FRONTIER")
         self.config = config
         self.to_be_downloaded = list()
+        self.ignorelist = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", 
+            "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", 
+            "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", 
+            "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", 
+            "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", 
+            "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", 
+            "our", "ours", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", ",", ".", "/", "?", ":", ";"]
         
         if not os.path.exists(self.config.save_file) and not restart:
             # Save file does not exist, but request to load save.
